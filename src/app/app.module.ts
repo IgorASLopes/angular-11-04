4683128d7816f89b1f080dd909ef05d9394c7cbd
igor <igoraslopes@gmail.com>
2018-04-11 21:44:44 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { ClienteComponent } from './cliente/cliente.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { ClienteService } from './service/cliente.service';
+import { HttpClient } from '@angular/common/http/src/client';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/domain/cliente.ts
@@ -0,0 +1,11 @@
+export interface Cliente{
+    id:string;
+    cpf:string;
+    nome: string;
+    endereco:string;
+    municipio:string;
+    estado:string;
+    telefone:string;
+    email:string;
+    senha:string;
+}
